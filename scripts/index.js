1c97f870d2500e335b7cad7f59b44b01773ef895
marupe-ambolt <74052166+marupe-ambolt@users.noreply.github.com>
2021-08-17 11:22:34 +0200

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -28,7 +28,7 @@ const getPrediction = async (image) => {
     image = prepareImage(image)
 
     // Send POST request
-    const response = await fetch('https://symposium-demo.northeurope.cloudapp.azure.com/facemask-detector/api/predict-webcam', {
+    const response = await fetch('https://symposium-demo.northeurope.cloudapp.azure.com/object-detector/api/object-detection', {
     // const response = await fetch('http://localhost:4242/api/predict-webcam', {
 
         method: 'POST',
