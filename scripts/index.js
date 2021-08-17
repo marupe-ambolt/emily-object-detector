30b8b50bfb8e0e4fc097de0f5821058ff3c11322
marupe-ambolt <74052166+marupe-ambolt@users.noreply.github.com>
2021-08-17 11:32:17 +0200

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -6,6 +6,7 @@ const webcam = new Webcam(webcamElement, 'user', canvasElement, snapSoundElement
 const [loadingIcon] = document.getElementsByClassName('loading-icon')
 const [statusMessage] = document.getElementsByClassName('status')
 const [webcamFeed] = document.getElementsByClassName('webcam-feed')
+const image = document.getElementById('image')
 
 const startWebcam = async () => {
     try {
@@ -28,64 +29,35 @@ const getPrediction = async (image) => {
     image = prepareImage(image)
 
     // Send POST request
-    const response = await fetch('https://symposium-demo.northeurope.cloudapp.azure.com/object-detector/api/object-detection', {
-    // const response = await fetch('http://localhost:4242/api/predict-webcam', {
-
+    const response = await fetch('http://localhost:4242/api/object-detection', {
         method: 'POST',
-        body: JSON.stringify({"image": image, "model_path": "data/models/resnet_18_epoch_153_acc_93.53.pth"})
+        body: JSON.stringify({"image": image})
     })
     const responseBody = await response.json()
-
-    console.log("Prediction: " + responseBody.prediction)
-    if (responseBody.prediction < 50) return 0
-    if (responseBody.prediction < 85) return 1
-    else return 2
+    
+    console.log("Response: " + responseBody.prediction)
+    return responseBody.prediction
 }
 
-const beginDetectionLoop = async () => {
+const beginLoop = async () => {
+    
     const picture = webcam.snap()
-    console.log(picture)
 
     while (true) {
 
         const picture = webcam.snap()
-        const hasMask = await getPrediction(picture);
-        console.log(`Got prediction for has mask: ${hasMask}`)
 
-        if (hasMask == null)
-            continue
+        // Send the image to the API
+        const imgAsBase64 = await getPrediction(picture);
 
-        loadingIcon.classList.add('disabled')
-        if (hasMask == 2) {
-            webcamFeed.classList.remove('warn')
-            statusMessage.classList.remove('warn')
-            webcamFeed.classList.add('success')
-            statusMessage.classList.add('success')
-
-            statusMessage.innerHTML = '✓ Mask found!'
-        } 
-        else if(hasMask == 1){
-            webcamFeed.classList.remove('success')
-            statusMessage.classList.remove('success')
-            webcamFeed.classList.add('warn')
-            statusMessage.classList.add('warn')
-            statusMessage.innerHTML = '? Not sure'
-        }
-        else {
-            webcamFeed.classList.remove('success')
-            statusMessage.classList.remove('success')
-            webcamFeed.classList.add('warn')
-            statusMessage.classList.add('warn')
-            statusMessage.innerHTML = '✗ No mask found'
-        }
-
-        // Set sleep timeout for 1/3 of a sec to avoid the CPU/GPU to be overflown with requests
-        await new Promise(r => setTimeout(r, 333));
+        // Display the image
+        image.setAttribute('src', `data:image/png;base64, ${imgAsBase64}`);
+    
     }
 }
 
 
 startWebcam()
     .then(async () => {
-        await beginDetectionLoop()
+        await beginLoop()
     })
